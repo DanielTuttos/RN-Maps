3d9ab84ef887c001c191db8dfb51d6fb1b67e688
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-04-08 07:59:30 -0500

    :construction: library configuration for permissions

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {Text, View} from 'react-native';
 import {StackNavigation} from './presentation/navigation/StackNavigation';
 
 export const MapsApp = () => {
